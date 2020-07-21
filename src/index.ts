be124a96b43678c9c0b32711f767e7e4f6a7d082
Mykola Harmash <mykola.harmash@gmail.com>
2020-07-21 16:04:22 +0200

    Add initial command use-cases separation

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,81 @@
-import {test} from './test'
+import { execSync } from 'child_process'
 
-enum Some {
-  Test,
-  Enother
+// Sub-commands
+
+function isSubCommand(name: string): boolean {
+  return ['init', 'list', 'new'].includes(name)
 }
 
-enum Test {  
-  A,  
-  B,  
-  C = Math.floor(Math.random() * 1000),  
-  D = 10,  
-  E  
+function executeSubCommand(name: string, args: string[]) {
+  switch (name) {
+    case 'init': {
+      init(args)
+      break
+    }
+    case 'list': {
+      list(args)
+      break
+    }
+    case 'new': {
+      create(args)
+      break
+    }
+    default: {
+      throw new Error(`Unknown sub-command ${name}`)
+    }
+  }
 }
 
-function some() {
-  return Promise.resolve('hello')
+function init(args: string[]) {
+  console.log('init sub-command')
 }
 
-async function main () {
-  const s = await some()
-
-  console.log(s)
+function list(args: string[]) {
+  console.log('list sub-command')
 }
 
-console.log(Some)
-test()
-main()
+function create(args: string[]) {
+  console.log('new sub-command')
+}
+
+// Bare
+
+function bare() {
+  console.log('Bare')
+}
+
+// Jump to a branch
+
+function jumpTo(args: string[]) {
+  console.log('Jumping to a branch')
+
+//   const argsString = args.join(' ')
+// const switchCommand = `git switch ${argsString}`
+
+// try {
+//   const output = execSync(switchCommand)
+
+//   console.log(output)
+// } catch (error) {
+//   console.log(error.stderr.toString())
+// }
+
+}
+
+function main(args: string[]) {
+  if (args.length === 0) {
+    bare()
+
+    return
+  }
+
+  if (isSubCommand(args[0])) {
+    executeSubCommand(args[0], args.slice(1))
+
+    return
+  }
+
+  jumpTo(args)  
+}
+
+main(process.argv.slice(2))
